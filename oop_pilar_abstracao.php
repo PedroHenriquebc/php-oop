af5b15b5df509e3439c08753b2f83e86d664c701
Pedro Henrique <pedrohbc96@gmail.com>
2023-10-01 17:11:34 -0300

    feat: adiciona getters e setters

--- oop_pilar_abstracao.php
+++ oop_pilar_abstracao.php
@@ -2,10 +2,35 @@
     //modelo
     class Funcionario {
         //atributos
-        public $nome = "Pedro";
-        public $telefone = "81 99999-8888";
-        public $numFilhos = 2;
+        public $nome = null;
+        public $telefone = null;
+        public $numFilhos = null;
         
+        //getter e setters
+        function setNome($nome) {
+            $this->nome = $nome;
+        }
+
+        function setNumFilhos($numFilhos) {
+            $this->numFilhos = $numFilhos;
+        }
+
+        function setTelefone($telefone) {
+            $this->telefone = $telefone;
+        }
+
+        function getNome() {
+            return $this->nome;
+        }
+
+        function getNumFilhos() {
+            return $this->numFilhos;
+        }
+
+        function getTelefone() {
+            return $this->telefone;
+        }
+
         //metódos
         function resumirCadFunc() {
             return "$this->nome possui $this->numFilhos filhos!";
@@ -17,8 +42,13 @@
     }
 
     $funcionario = new Funcionario();
+    $funcionario->setNome("Pedro");
+    $funcionario->setNumFilhos(1);
+    $funcionario->setTelefone("81 9999-8888");
     print($funcionario->resumirCadFunc());
     $funcionario->modificarNumFilhos(4);
     print "<br/>";
     print($funcionario->resumirCadFunc());
+    print "<br/>";
+    print($funcionario->getNome() . " possui " . $funcionario->getNumFilhos() . " e seu telefone é: " . $funcionario->getTelefone());
 ?>
